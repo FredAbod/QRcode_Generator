4d2cd29589faf5465ace0f84c7a3cf7365ec25c4
FredAbod <fredrickbolutife@gmail.com>
2022-09-08 07:13:50 +0100

    editing

--- qrcode_image.js
+++ qrcode_image.js
@@ -3,10 +3,10 @@ const QRCode = require('qrcode');
 
 const generateQR = async text => {
 	try {
-		await QRCode.toFile('./yahoo-qr-code.png', text);
+		await QRCode.toFile('./qr-code.png', text);
 	} catch(err){
 		console.log(err);
 	}
 }
 
-generateQR("https://github.com/FredAbod/My_First_Challenge/");
+generateQR("https://github.com/FredAbod/QRcode_Generator");
